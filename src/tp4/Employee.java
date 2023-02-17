0798e4598487b89ce0c00e057aa7a99ec9b782f4
s22009730 <s22009730@A-F001617.salsa.univ-amu.fr>
2023-02-17 16:57:35 +0100

    seance 2

---
+++ src/tp4/Employee.java
@@ -0,0 +1,49 @@
+package tp4;
+
+public class Employee {
+private String name;
+private String firstname;
+private String lastname;
+private int salary;
+private int id;
+private int AnnualSalary;
+private int raiseSalary;
+
+public Employee(int id, int sal, String fn, String ln) {
+	this.salary= sal;
+	this.firstname=fn;
+	this.id=id;
+	this.lastname=ln;
+	this.name=ln+fn;
+}
+public int getSalary(){
+	return salary;
+}
+
+public int getID(){
+	return id;
+}
+
+public String getFirstName(){
+	return firstname;
+}
+
+public String getLastName(){
+	return lastname;
+}
+
+public String getName(){
+	return name;
+}
+
+public double getAnnualSalary(){
+	return salary*12;
+}
+public void SetSalary(int salary) {
+	this.salary=salary;
+}
+public int raiseSalary( int percent) {
+	return this.raiseSalary= (salary * percent)/100;
+}
+
+}
