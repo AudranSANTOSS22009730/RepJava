6f387ca3acb226470bc56ec565973c72de072adc
s22009730 <s22009730@A-F001617.salsa.univ-amu.fr>
2023-02-17 17:29:16 +0100

    seance 2

--- src/tp4/Circle.java
+++ src/tp4/Circle.java
@@ -1,5 +1,31 @@
 package tp4;
+public class Circle { 
 
-public class Circle {
+	   private double radius;
+	   private String color;
 
-}
+	   public Circle(double r, String c) {  
+	      radius = r;
+	      color = c;
+	   }
+	   
+	   public String getColor() {
+		   return color; 
+	   }
+
+	   
+	   public Circle(double r) {  
+	      radius = r;
+	      color = "red";
+	   }
+	   
+
+	   public double getRadius() {
+	     return radius; 
+	   }
+	   
+
+	   public double getArea() {
+	      return radius*radius*Math.PI;
+	   }
+	}
